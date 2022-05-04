a5c8401da6dc786b25412e9350c580c26b200a46
Felipe <fellipefreiire3@gmail.com>
2022-05-03 22:49:55 -0300

    initial commit

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from 'axios'
+
+export const CommentCreate = ({ postId }) => {
+  const [content, setContent] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content
+    })
+
+    setContent('')
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="input-c">New Comment</label>
+          <input
+            type="text"
+            id='input-c'
+            className="form-control"
+            value={content}
+            onChange={e => setContent(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  )
+}
---
+++ client/src/PostCreate.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export const PostCreate = () => {
+  const [title, setTitle] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios.post('http://localhost:4000/posts', {
+      title
+    })
+
+    setTitle('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label htmlFor="input">Title</label>
+          <input
+            id='input'
+            type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  )
+}
