2e047ffa19d3f7105662f5a1fe8bab745615ac18
Felipe <fellipefreiire3@gmail.com>
2022-05-04 01:42:26 -0300

    finished

--- posts/index.js
+++ posts/index.js
@@ -14,7 +14,7 @@ app.get('/posts', (req, res) => {
   res.send(posts)
 })
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const id = randomBytes(4).toString('hex')
   const { title } = req.body
 
@@ -43,6 +43,6 @@ app.post('/events', async (req, res) => {
 })
 
 app.listen(4000, () => {
-  console.log('v56')
+  console.log('v1000')
   console.log('Listening on 4000')
 })
